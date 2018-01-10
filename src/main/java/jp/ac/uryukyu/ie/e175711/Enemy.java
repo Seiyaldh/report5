730885575f64692325ab57b79e4273ae2aacd500
Seiya Nakamura <e175711@ie.u-ryukyu.ac.jp>
2018-01-11 02:13:42 +0900

    third commit

--- src/main/java/jp/ac/uryukyu/ie/e175711/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175711/Enemy.java
@@ -4,6 +4,23 @@ public class Enemy extends LivingThing {
     public Enemy(String name, int maximumHP, int attack) {
         super(name,maximumHP,attack);
     }
+    @Override
+    public void attack(LivingThing opponent) {
+        int tuu = (int) (Math.random() * 10) + 1;
+        if (!getDead()) {
+            int damage = (int) (Math.random() * getAttack());
+            if (damage == 0) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+            }
+            if (damage != 0 && (tuu - 3) <= 0) {
+                damage = damage * 2;
+                System.out.printf("%sの攻撃！痛恨の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+            } else if (damage != 0 && tuu - 3 > 0) {
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+            }
+            opponent.wounded(damage);
+        }
+    }
     public void wounded(int damage) {
          setHitPoint(getHitPoint()-damage);
         if(getHitPoint() < 0) {
--- src/main/java/jp/ac/uryukyu/ie/e175711/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e175711/Hero.java
@@ -4,6 +4,23 @@ public class Hero extends LivingThing{
     public Hero(String name, int maximumHP, int attack) {
         super(name,maximumHP,attack) ;
     }
+    @Override
+    public void attack(LivingThing opponent) {
+        int kai = (int)(Math.random()*10)+1;
+        if (!getDead()) {
+            int damage = (int) (Math.random() * getAttack());
+            if (damage == 0) {
+                System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), opponent.getName());
+            }
+            if (damage != 0 && (kai-4) <= 0) {
+                damage = damage*2;
+                System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+            } else if (damage != 0 && kai - 4 > 0){
+                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), opponent.getName(), damage);
+            }
+            opponent.wounded(damage);
+        }
+    }
     public void wounded(int damage) {
         setHitPoint(getHitPoint()-damage);
         if(getHitPoint() < 0) {
